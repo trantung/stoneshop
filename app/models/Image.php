1ee8f90aee0cbd8a62f4d87badea8b3f324b35c7
thaihv <thaihv@nadia.bz>
2015-06-01 11:36:49 +0700

    complete image manager

--- app/models/Image.php
+++ app/models/Image.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class Image extends Eloquent {
+	use SoftDeletingTrait;
     protected $table = 'images';
+	protected $fillable = array('type', 'description','image_url', 'status');
+    protected $dates = ['deleted_at'];
 
 }
--- app/views/admin/image_edit.blade.php
+++ app/views/admin/image_edit.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.admin.header')
 
 @section('content')
+<?php 
+	$type_selected="";
+	$status_selected = "";
+	if($image->type ==2){
+		$type_selected = "Footer";
+	}
+	if($image->type ==3){
+		$type_selected="Logo";
+	}
+	if($image->status == 2){
+		$status_selected = "None";
+	}
+?>
 <div class="form-comment" style="margin-right:10%">
-	{{Form::open(array("route"=>array('image.post.create'),"class"=>"form-horizontal",'files'=>true))}}
+	{{Form::open(array("route"=>array('image.post.edit', $image->id),"class"=>"form-horizontal",'files'=>true))}}
 	    <div class="form-group">
 	        {{Form::label('lblName',"Type", array("class"=>"col-sm-2 control-label"))}}
 	        <div class="col-sm-10">
@@ -10,31 +23,37 @@
 				   '1' => 'Header',
 				   '2' => 'Footer',
 				   '3' => 'Logo'],
-				   'Header',
+				   $type_selected,
 			   [	"class"=>"form-control"]) }}
 	        </div>
 	    </div>
 	     <div class="form-group">
 	        	{{Form::label('lblDescript',"Description", array("class"=>"col-sm-2 control-label"))}}
 	        <div class="col-sm-10">
-	        	{{Form::textarea('description',"", array('class'=>'form-control',"rows"=>6))}}
+	        	{{Form::textarea('description',$image->description, array('class'=>'form-control',"rows"=>6))}}
 	        </div>
 	    </div>
 	    <div class="form-group">
         	{{Form::label('lblAddress',"status", array("class"=>"col-sm-2 control-label"))}}
 	        <div class="col-sm-10">
 		        {{ Form::select('status', [
-				   '1' => 'use',
-				   '3' => 'none'],
-				   'none',
+				   '1' => 'Use',
+				   '2' => 'None'],
+				   $status_selected,
 			   [	"class"=>"form-control"]) }}
 	        </div>
 	    </div>
 	    <div class="form-group">
+	    <?php 
+	        	$image_url = $image->image_url;
+	        	if($image->image_url == null){
+	        		$image_url = 'nothumnail.jpg';
+	        	}
+	        ?>
 	        {{Form::label('lbImage',"Image", array("class"=>"col-sm-2 control-label"))}}
 	        <div class="col-sm-10">
 	        	{{Form::file('image',"", array('class'=>'form-control','id'=>'imgInp'))}}
-	        	<img src="{{asset('img/nothumnail.jpg')}}" class="img-rounded" alt="Cinque Terre" width="304" height="236" id="blah">
+	        	<img src="{{asset('img/headers').'/'.$image_url}}" class="img-rounded" alt="Cinque Terre" width="304" height="236" id="blah">
 	        </div>
 	    </div>
 	    <div class="form-group">
@@ -44,7 +63,6 @@
 	    </div>
 	{{Form::close()}}
 </div>
-
 	<script type="text/javascript">
 		    function readURL(input) {
 		        if (input.files && input.files[0]) {
@@ -60,4 +78,3 @@
     });
 	</script>
 @stop
-
