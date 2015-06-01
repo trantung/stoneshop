38b55de24e1084d1de13fe4daba22d2dd553f6a1
thaihv <thaihv@nadia.bz>
2015-06-01 17:27:34 +0700

    Counter visited checked yet

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -2,8 +2,38 @@
 
 @section('content')
 <div class="manage-menu info-user-td"><br />
-    <div class="att">
-            Demo 
+
+     <section class="content">
+            <div class="row">
+            <div class="col-lg-4 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                  <h3>150</h3>
+                  <center>User Vote</center>
+                </div>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-4 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3>{{$userOnline}}</sup></h3>
+                  <center>User Online</center>
+                </div>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-4 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-yellow">
+                <div class="inner">
+                  <h3>44</h3>
+                  <center>User Visited</center>
+                </div>
+              </div>
+            </div><!-- ./col -->
+          </div><!-- /.row -->
+         </section>
     </div>
         
 </div>
