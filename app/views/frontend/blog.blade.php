9c513f875851ebb09f0044686ad87ce6dde6579a
thaihv <louisthaihv@gmail.com>
2015-06-21 03:28:08 +0700

    design blog

--- app/views/frontend/blog.blade.php
+++ app/views/frontend/blog.blade.php
@@ -2,5 +2,191 @@
 
 @section('content')
 	<center><h3>Welcome to my blog!</h3></center>
-	<p>blog content</p>
+
+<div class="row">
+	<div class="col-xs-8">
+	<div class="row">
+			<div class="col-xs-4 col-sm-3">
+				<div class="post-meta">
+					<a href="http://demo2.woothemes.com/mystile/author/wooadmin/">
+					<img class="img-circle" height="128" width="128" srcset="http://1.gravatar.com/avatar/4356450f3c00bd71eacd6afd0018549f?s=256&d=mm&r=g 2x" src="http://1.gravatar.com/avatar/4356450f3c00bd71eacd6afd0018549f?s=128&d=mm&r=g" alt="">
+					</a>
+					<span class="month">Oct</span>
+					<span class="day">20</span>
+					<span class="year">2011</span>
+				</div>
+			</div>
+			<div class="col-xs-8 col-sm-9">
+			<img src="http://farm3.static.flickr.com/2557/4131103966_6e0288719c_b.jpg" alt="Image in a post"  title="Image in a post" class="thumbnail">
+			<h1>
+				<a href="http://demo2.woothemes.com/mystile/2009/11/11/sed-tincidunt-augue-et-nibh/" rel="bookmark" title="Sed tincidunt augue et nibh">Title of Post</a>
+			</h1>
+				<p>
+					The contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe content
+				</p>
+		<div class="line-space">&nbsp;</div>
+		</div>
+	</div>
+	
+		<div class="row">
+			<div class="col-xs-4 col-sm-3">
+				<div class="post-meta">
+					<a href="http://demo2.woothemes.com/mystile/author/wooadmin/">
+					<img class="img-circle" height="128" width="128" srcset="http://1.gravatar.com/avatar/4356450f3c00bd71eacd6afd0018549f?s=256&d=mm&r=g 2x" src="http://1.gravatar.com/avatar/4356450f3c00bd71eacd6afd0018549f?s=128&d=mm&r=g" alt="">
+					</a>
+					<span class="month">Oct</span>
+					<span class="day">20</span>
+					<span class="year">2011</span>
+				</div>
+			</div>
+			<div class="col-xs-8 col-sm-9">
+			<img src="http://farm3.static.flickr.com/2557/4131103966_6e0288719c_b.jpg" alt="Image in a post"  title="Image in a post" class="thumbnail">
+			<h1>
+				<a href="http://demo2.woothemes.com/mystile/2009/11/11/sed-tincidunt-augue-et-nibh/" rel="bookmark" title="Sed tincidunt augue et nibh">Title of Post</a>
+			</h1>
+				<p>
+					The contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe contentThe content
+				</p>
+				<div class="line-space">&nbsp;</div>
+		</div>
+	</div>
+	<nav class="paginate">
+		  <ul class="pagination">
+		    <li>
+		      <a href="#" aria-label="Previous">
+		        <span aria-hidden="true">&laquo;</span>
+		      </a>
+		    </li>
+		    <li><a href="#">1</a></li>
+		    <li><a href="#">2</a></li>
+		    <li><a href="#">3</a></li>
+		    <li><a href="#">4</a></li>
+		    <li><a href="#">5</a></li>
+		    <li>
+		      <a href="#" aria-label="Next">
+		        <span aria-hidden="true">&raquo;</span>
+		      </a>
+		    </li>
+		  </ul>
+	</nav>
+	</div>
+	<div class="col-xs-4">
+		<ul class="nav nav-tabs">
+		    <li class="active"><a href="#tab1" data-toggle="tab">Lastest</a></li>
+		    <li><a href="#tab2" data-toggle="tab">Popular</a></li>
+		     <li><a href="#tab3" data-toggle="tab">Comment</a></li>
+  		</ul>
+  		<div class="tab-content lastest-comment">
+		    <div class="tab-pane active" id="tab1">
+		      	<ul>
+					<li>
+						<div class="row">
+							<div class="col-xs-2">
+								<a href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/" title="Testing The Elements" class="post-meta">
+									<img src="http://farm3.static.flickr.com/2583/4131104704_31fb82ce85_b.jpg" alt="Testing The Elements" width="45" height="45" title="Testing The Elements">
+								</a>
+							</div>
+							<div class="col-xs-10">
+								<a title="Testing The Elements" href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/">
+									Lastest 1
+								</a>
+							<span class="meta">January 11, 2010</span>
+						</div>
+						</div>
+
+					</li>
+
+					<li>
+						<div class="row">
+							<div class="col-xs-2">
+								<a href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/" title="Testing The Elements" class="post-meta">
+									<img src="http://farm3.static.flickr.com/2583/4131104704_31fb82ce85_b.jpg" alt="Testing The Elements" width="45" height="45" title="Testing The Elements">
+								</a>
+							</div>
+							<div class="col-xs-10">
+								<a title="Testing The Elements" href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/">
+									Lastest 2
+								</a>
+							<span class="meta">January 11, 2010</span>
+						</div>
+						</div>
+					</li>
+				</ul>
+		    </div>
+		    <div class="tab-pane" id="tab2">
+		      <ul class="popular">
+					<li>
+						<div class="row">
+							<div class="col-xs-2">
+								<a href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/" title="Testing The Elements" class="post-meta">
+									<img src="http://farm3.static.flickr.com/2583/4131104704_31fb82ce85_b.jpg" alt="Testing The Elements" width="45" height="45" title="Testing The Elements">
+								</a>
+							</div>
+							<div class="col-xs-10">
+								<a title="Testing The Elements" href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/">
+									Popular 1
+								</a>
+							<span class="meta">January 11, 2010</span>
+						</div>
+						</div>
+						
+					</li>
+
+					<li>
+						<div class="row">
+							<div class="col-xs-2">
+								<a href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/" title="Testing The Elements" class="post-meta">
+									<img src="http://farm3.static.flickr.com/2583/4131104704_31fb82ce85_b.jpg" alt="Testing The Elements" width="45" height="45" title="Testing The Elements">
+								</a>
+							</div>
+							<div class="col-xs-10">
+								<a title="Testing The Elements" href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/">
+									Popular 2
+								</a>
+							<span class="meta">January 11, 2010</span>
+						</div>
+						</div>
+					</li>
+				</ul>
+		    </div>
+		    <div class="tab-pane" id="tab3">
+		      <ul class="popular">
+					<li>
+						<div class="row">
+							<div class="col-xs-2">
+								<a href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/" title="Testing The Elements" class="post-meta">
+									<img src="http://farm3.static.flickr.com/2583/4131104704_31fb82ce85_b.jpg" alt="Testing The Elements" width="45" height="45" title="Testing The Elements">
+								</a>
+							</div>
+							<div class="col-xs-10">
+								<a title="Testing The Elements" href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/">
+									Comment 1
+								</a>
+							<span class="meta">January 11, 2010</span>
+						</div>
+						</div>
+						
+					</li>
+
+					<li>
+						<div class="row">
+							<div class="col-xs-2">
+								<a href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/" title="Testing The Elements" class="post-meta">
+									<img src="http://farm3.static.flickr.com/2583/4131104704_31fb82ce85_b.jpg" alt="Testing The Elements" width="45" height="45" title="Testing The Elements">
+								</a>
+							</div>
+							<div class="col-xs-10">
+								<a title="Testing The Elements" href="http://demo2.woothemes.com/mystile/2010/01/11/testing-the-elements/">
+									Comment 2
+								</a>
+							<span class="meta">January 11, 2010</span>
+						</div>
+						</div>
+					</li>
+				</ul>
+		    </div>
+  		</div>
+	</div>
+
+</div>
 @stop
