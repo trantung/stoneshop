cf034bac92bdc3a38877f69dd731903dd2c57209
Tran Thanh Tung <tranthanhtung@Trans-MacBook-Pro.local>
2015-06-21 16:34:06 +0700

    fix footer

--- app/views/layouts/frontend/footer.blade.php
+++ app/views/layouts/frontend/footer.blade.php
@@ -1,11 +1,7 @@
 	<footer class="container-fluid">
 		<a href="#"><img src="{{asset('img/headers').'/'.$footer}}" alt="SHOP STONE" title="SHOP STONE"/></a>
 		<div class="fb-like" data-href="{{Request::url()}}" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
-		<ul>
-			<li><a href="{{route('frontend.index')}}">Trang chủ</a></li>
-			<li><a href="{{route('frontend.blog')}}">Bài viết</a></li>
-			<li class="last"><a href="{{route('frontend.aboutUs')}}"> Liên hệ</a></li>
-		</ul>
+		
 	</footer>
 </body>
 </html>
