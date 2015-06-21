33f04a89d2aecd3f094fa0c890ad38d902e5fea5
Tran Thanh Tung <tranthanhtung@Trans-MacBook-Pro.local>
2015-06-21 17:13:34 +0700

    fix migration for blogdescription

--- app/database/seeds/BlogDescriptionTableSeeder.php
+++ app/database/seeds/BlogDescriptionTableSeeder.php
@@ -7,28 +7,11 @@ class BlogDescriptionTableSeeder extends Seeder {
         BlogDescription::insert(
                         array(
                         'user_id'=>1,
-                        'title'=>'title1'
+                        'title'=>'title1',
                         'description'=>'day la chi tiet blog so 1 cua group1',
                         'created_at'=>$now,
-                        'updated_at'=>$now,
+                        'updated_at'=>$now
                         ));
-        BlogDescription::insert(
-                        array(
-                        'user_id'=>1,
-                        'title'=>'title2'
-                        'description'=>'day la chi tiet blog so 2 cua group1',
-                        'created_at'=>$now,
-                        'updated_at'=>$now,
-                        ));
-        BlogDescription::insert(
-                        array(
-                        'user_id'=>2,
-                        'title'=>'title3'
-                        'description'=>'day la chi tiet blog so 1 cua group2',
-                        'created_at'=>$now,
-                        'updated_at'=>$now,
-                        )
-        );
 
     }
 }
